ec58ae07bc2c60a8498111a0462cda4354e25750
Jens Reimann <ctron@dentrassi.de>
2023-12-20 18:28:07 +0100

    build: change version to 0.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "de.dentrassi.vat.nfc.programmer"
         minSdk = 29
         targetSdk = 33
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
