aed1fd2c658d69a321b1b058ef2b62d67104010b
Jens Reimann <ctron@dentrassi.de>
2024-01-06 18:00:00 +0100

    build: uptick version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "de.dentrassi.vat.nfc.programmer"
         minSdk = 29
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.0.3"
+        versionCode = 4
+        versionName = "0.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
