b52d725e7b2d88f58731ad216427cea37ca7a234
Jens Reimann <ctron@dentrassi.de>
2024-01-13 19:55:53 +0100

    chore: uptick version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "de.dentrassi.vat.nfc.programmer"
         minSdk = 29
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
